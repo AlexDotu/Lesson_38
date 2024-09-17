528d35f1d5e35ed3aca5cf1333e1322d8152318f
Aleksandr Dotsu <51963885+AlexDotu@users.noreply.github.com>
2024-09-17 21:28:42 +0200

    Delete string_utils directory

--- string_utils/main.py
+++
@@ -1,7 +0,0 @@
-import sring_operation, string_analysis
-
-string = input("Enter your string: ")
-sring_operation.reverse_string(string)
-sring_operation.capitalize_words(string)
-string_analysis.count_char(string)
-string_analysis.count_vowels(string)
